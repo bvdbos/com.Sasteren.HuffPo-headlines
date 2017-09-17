740ef2d6ea4784b0daa33b5e630001a52ff76973
Bas van den Bosch <bvdbos@gmail.com>
2017-09-17 16:30:15 +0200

    version1
    tweakers vervangen door huffpo

--- app.js
+++ app.js
@@ -3,283 +3,327 @@
 const Homey = require('homey');
 const feedparser = require('feedparser-promised');
 
-class Tweakers extends Homey.App {
+class Huffpost extends Homey.App {
 
     onInit() {
 
-        this.log('Tweakers is gestart');
+        this.log('Huffington Post is gestart');
 
         //App instantie variablen
-        this.TweakersMixed = null;
-        this.TweakersNieuws = null;
-        this.TweakersMeukTracker = null;
-        this.TweakersGames = null;
-        this.TweakersVraagAanbod = null;
+        this.HuffPoWorldNews = null;
+        this.HuffPoBusiness = null;
+        this.HuffPoEntertainment = null;
+        this.HuffPoSports = null;
+        this.HuffPoBooks = null;
+		this.HuffPoGreen = null;
 
         //Flow card registration 
-        this.TriggerTweakersMixed = new Homey.FlowCardTrigger('TriggerTweakersMixed')
+		//to do via settings uitkiezen welke gepolled moeten worden
+        this.TriggerHuffPoWorldNews = new Homey.FlowCardTrigger('TriggerHuffPoWorldNews')
             .register();
-        this.TriggerTweakersNieuws = new Homey.FlowCardTrigger('TriggerTweakersNieuws')
+        this.TriggerHuffPoBusiness = new Homey.FlowCardTrigger('TriggerHuffPoBusiness')
             .register();
-        this.TriggerTweakersMeukTracker = new Homey.FlowCardTrigger('TriggerTweakersMeukTracker')
+        this.TriggerHuffPoEntertainment = new Homey.FlowCardTrigger('TriggerHuffPoEntertainment')
             .register();
-        this.TriggerTweakersGames = new Homey.FlowCardTrigger('TriggerTweakersGames')
+        this.TriggerHuffPoSports = new Homey.FlowCardTrigger('TriggerHuffPoSports')
             .register();
-        this.TriggerTweakersVraagAanbod = new Homey.FlowCardTrigger('TriggerTweakersVraagAanbod')
+        this.TriggerHuffPoBooks = new Homey.FlowCardTrigger('TriggerHuffPoBooks')
             .register();
-
+        this.TriggerHuffPoGreen = new Homey.FlowCardTrigger('TriggerHuffPoGreen')
+            .register();
+			
         //Start loop interval
         this.log('StartInterval');
         this.startLoop();
 
     }
 
-    //Tweakers homey app
 
-    //Function LoopTweakers
-    async LoopTweakers() {
-        this.log('Start LoopTweakers');
-        await this.GetTweakersMixed();
-        await this.GetTweakersNieuws();
-        await this.GetTweakersMeukTracker();
-        await this.GetTweakersGames();
-        await this.GetTweakersVraagAanbod();
-        this.log('Einde LoopTweakers');
+    //Function LoopHuffPo
+	//to do via settings uitkiezen welke gepolled moeten worden
+    async LoopHuffPo() {
+        this.log('Start LoopHuffPo');
+        await this.GetHuffPoWorldNews();
+        await this.GetHuffPoBusiness();
+        await this.GetHuffPoEntertainment();
+        await this.GetHuffPoSports();
+        await this.GetHuffPoBooks();
+        await this.GetHuffPoGreen();		
+        this.log('Einde LoopHuffPo');
     }
 
     //StartLoop
+	//todo maak hier een setting van
     startLoop() {
         setInterval(() => {
-            this.LoopTweakers();
+            this.LoopHuffPo();
         }, 300000); //5 minuten ->  300000
-        // Call this.loopTweakers() to set the initial state
-        this.LoopTweakers();
+        // Call this.loopHuffPo() to set the initial state
+        this.LoopHuffPo();
     }
 
-    //Trigger TweakersMixed
-    async GetTweakersMixed() {
+    //Trigger HuffPo WorldNews
+    async GetHuffPoWorldNews() {
         try {
 
-            const url = 'http://feeds.feedburner.com/tweakers/mixed';
+            const url = 'http://www.huffingtonpost.com/section/world-news/feed';
 
-            this.log('Start GetTweakersMixed');
+            this.log('Start GetHuffPoWorldNews');
 
             var items = await feedparser.parse(url);
-            var TweakersMixedTitel = items[0].title;
-            var TweakersMixedCategorie = items[0].categories;
-            var TweakersMixedDescription = items[0].description;
-            var TweakersMixedDatumTZ = items[0].date;
-            var TweakersMixedDatum = Date.parse(TweakersMixedDatumTZ) / 1000
-            this.log('End Success GetTweakersMixed');
+            var HuffPoWorldNewsTitle = items[0].title;
+            var HuffPoWorldNewsDescription = items[0].description;
+            var HuffPoWorldNewsDatumTZ = items[0].pubDate;
+            var HuffPoWorldNewsDatum = Date.parse(HuffPoWorldNewsDatumTZ) / 1000
+            this.log('End Success GetHuffPoWorldNews');
 
         } catch (error) {
             console.error('error: ', error)
-            this.log('End Error GetTweakersMixed');
+            this.log('End Error GetHuffPoWorldNews');
         }
 
-        if (this.TweakersMixed !== TweakersMixedDatum) {
-            this.log('IF timestamp:', this.TweakersMixed + ' = ' + 'TweakersMixedDatum:', TweakersMixedDatum);
-            this.TweakersMixed = TweakersMixedDatum;
-            console.log('IF titel:', TweakersMixedTitel);
-            console.log('IF timestamp update:', this.TweakersMixed);
+        if (this.HuffPoWorldNews !== HuffPoWorldNewsDatum) {
+            this.log('IF timestamp:', this.HuffPoWorldNews + ' = ' + 'HuffPoWorldNewsDatum:', HuffPoWorldNewsDatum);
+            this.HuffPoWorldNews = HuffPoWorldNewsDatum;
+            console.log('IF titel:', HuffPoWorldNewsTitle);
+            console.log('IF timestamp update:', this.HuffPoWorldNews);
 
             //trigger flow
             let tokens = {
-                'title': TweakersMixedTitel,
-                'description': TweakersMixedDescription
+                'title': HuffPoWorldNewsTitle,
+                'description': HuffPoWorldNewsDescription
             }
 
-            return this.TriggerTweakersMixed
+            return this.TriggerHuffPoWorldNews
                 .trigger(tokens)
                 .catch(this.error)
                 .then(this.log)
 
         } else {
-            this.log('ELSE timestamp:', this.TweakersMixed + ' = ' + 'TweakersMixedDatum:', TweakersMixedDatum);
+            this.log('ELSE timestamp:', this.HuffPoWorldNews + ' = ' + 'HuffPoWorldNewsDatum:', HuffPoWorldNewsDatum);
         }
 
     }
 
 
-    //Trigger TweakersNieuws
-    async GetTweakersNieuws() {
+    //Trigger HuffPoBusiness
+    async GetHuffPoBusiness() {
         try {
 
-            const url = 'http://feeds.feedburner.com/tweakers/nieuws';
+            const url = 'http://www.huffingtonpost.com/section/business/feed';
 
-            this.log('Start GetTweakersNieuws');
+            this.log('Start GetHuffPoBusiness');
 
             var items = await feedparser.parse(url);
-            var TweakersNieuwsTitel = items[0].title;
-            var TweakersNieuwsDescription = items[0].description;
-            var TweakersNieuwsCategories = items[0].categories;
-            var TweakersNieuwsDatumTZ = items[0].date;
-            var TweakersNieuwsDatum = Date.parse(TweakersNieuwsDatumTZ) / 1000
-            this.log('End Success GetTweakersNieuws');
+            var HuffPoBusinessTitle = items[0].title;
+            var HuffPoBusinessDescription = items[0].description;
+            var HuffPoBusinessDatumTZ = items[0].pubDate;
+            var HuffPoBusinessDatum = Date.parse(HuffPoBusinessDatumTZ) / 1000
+            this.log('End Success GetHuffPoBusiness');
 
         } catch (error) {
             console.error('error: ', error)
-            this.log('End Error GetTweakersNieuws');
+            this.log('End Error GetHuffPoBusiness');
         }
 
-        if (this.TweakersNieuws !== TweakersNieuwsDatum) {
-            this.log('IF timestamp:', this.TweakersNieuws + ' = ' + 'TweakersNieuwsDatum:', TweakersNieuwsDatum);
-            this.TweakersNieuws = TweakersNieuwsDatum;
-            console.log('IF titel:', TweakersNieuwsTitel);
-            console.log('IF timestamp update:', this.TweakersNieuws);
+        if (this.HuffPoBusiness !== HuffPoBusinessDatum) {
+            this.log('IF timestamp:', this.HuffPoBusiness + ' = ' + 'HuffPoBusinessDatum:', HuffPoBusinessDatum);
+            this.HuffPoBusiness = HuffPoBusinessDatum;
+            console.log('IF titel:', HuffPoBusinessTitle);
+            console.log('IF timestamp update:', this.HuffPoBusiness);
 
             //trigger flow
             let tokens = {
-                'title': TweakersNieuwsTitel,
-                'description': TweakersNieuwsDescription
+                'title': HuffPoBusinessTitle,
+                'description': HuffPoBusinessDescription
             }
 
 
 
-            return this.TriggerTweakersNieuws
+            return this.TriggerHuffPoBusiness
                 .trigger(tokens)
                 .catch(this.error)
                 .then(this.log)
 
         } else {
-            this.log('ELSE timestamp:', this.TweakersNieuws + ' = ' + 'TweakersNieuwsDatum:', TweakersNieuwsDatum);
+            this.log('ELSE timestamp:', this.HuffPoBusiness + ' = ' + 'HuffPoBusinessDatum:', HuffPoBusinessDatum);
         }
 
     }
 
-    //Trigger TweakersMeukTracker
-    async GetTweakersMeukTracker() {
+    //Trigger HuffPoEntertainment
+    async GetHuffPoEntertainment() {
         try {
 
-            const url = 'http://feeds.feedburner.com/tweakers/meuktracker';
+            const url = 'http://www.huffingtonpost.com/dept/entertainment/feed';
 
-            this.log('Start GetTweakersMeukTracker');
+            this.log('Start GetHuffPoEntertainment');
 
             var items = await feedparser.parse(url);
-            var TweakersMeukTitel = items[0].title;
-            var TweakersMeukCategorie = items[0].categories;
-            var TweakersMeukDescription = items[0].description;
-            var TweakersMeukDatumTZ = items[0].date;
-            var TweakersMeukDatum = Date.parse(TweakersMeukDatumTZ) / 1000
-            this.log('End Success GetTweakersMeukTracker');
+            var HuffPoEntertainmentTitle = items[0].title;
+            var HuffPoEntertainmentCategorie = items[0].categories;
+            var HuffPoEntertainmentDescription = items[0].description;
+            var HuffPoEntertainmentDatumTZ = items[0].pubDate;
+            var HuffPoEntertainmentDatum = Date.parse(HuffPoEntertainmentDatumTZ) / 1000
+            this.log('End Success GetHuffPoEntertainment');
 
         } catch (error) {
             console.error('error: ', error)
-            this.log('End Error GetTweakersMeukTracker');
+            this.log('End Error GetHuffPoEntertainment');
         }
 
-        if (this.TweakersMeukTracker !== TweakersMeukDatum) {
-            this.log('IF timestamp:', this.TweakersMeukTracker + ' = ' + 'TweakersMeukDatum:', TweakersMeukDatum);
-            this.TweakersMeukTracker = TweakersMeukDatum;
-            console.log('IF titel:', TweakersMeukTitel);
-            console.log('IF timestamp update:', this.TweakersMeukTracker);
+        if (this.HuffPoEntertainment !== HuffPoEntertainmentDatum) {
+            this.log('IF timestamp:', this.HuffPoEntertainment + ' = ' + 'HuffPoEntertainmentDatum:', HuffPoEntertainmentDatum);
+            this.HuffPoEntertainment = HuffPoEntertainmentDatum;
+            console.log('IF Title:', HuffPoEntertainmentTitle);
+            console.log('IF timestamp update:', this.HuffPoEntertainment);
 
             //trigger flow
             let tokens = {
-                'title': TweakersMeukTitel,
-                'description': TweakersMeukDescription
+                'title': HuffPoEntertainmentTitle,
+                'description': HuffPoEntertainmentDescription
             }
 
-            return this.TriggerTweakersMeukTracker
+            return this.TriggerHuffPoEntertainment
                 .trigger(tokens)
                 .catch(this.error)
                 .then(this.log)
 
         } else {
-            this.log('ELSE timestamp:', this.TweakersMeukTracker + ' = ' + 'TweakersMeukDatum:', TweakersMeukDatum);
+            this.log('ELSE timestamp:', this.HuffPoEntertainment + ' = ' + 'HuffPoEntertainmentDatum:', HuffPoEntertainmentDatum);
         }
 
     }
 
-    //Trigger TweakersGames
-    async GetTweakersGames() {
+    //Trigger HuffPoSports
+    async GetHuffPoSports() {
         try {
 
-            const url = 'http://feeds.feedburner.com/tweakers/games';
+            const url = 'http://www.huffingtonpost.com/section/sports/feed';
 
-            this.log('Start GetTweakersGames');
+            this.log('Start GetHuffPoSports');
 
             var items = await feedparser.parse(url);
-            var TweakersGamesTitel = items[0].title;
-            var TweakersGamesCategorie = items[0].categories;
-            var TweakersGamesDescription = items[0].description;
-            var TweakersGamesDatumTZ = items[0].date;
-            var TweakersGamesDatum = Date.parse(TweakersGamesDatumTZ) / 1000
-            this.log('End Success GetTweakersGames');
+            var HuffPoSportsTitle = items[0].title;
+            var HuffPoSportsDescription = items[0].description;
+            var HuffPoSportsDatumTZ = items[0].pubDate;
+            var HuffPoSportsDatum = Date.parse(HuffPoSportsDatumTZ) / 1000
+            this.log('End Success GetHuffPoSports');
 
         } catch (error) {
             console.error('error: ', error)
-            this.log('End Error GetTweakersGames');
+            this.log('End Error GetHuffPoSports');
         }
 
-        if (this.TweakersGames !== TweakersGamesDatum) {
-            this.log('IF timestamp:', this.TweakersGames + ' = ' + 'TweakersGamesDatum:', TweakersGamesDatum);
-            this.TweakersGames = TweakersGamesDatum;
-            console.log('IF titel:', TweakersGamesTitel);
-            console.log('IF timestamp update:', this.TweakersGames);
+        if (this.HuffPoSports !== HuffPoSportsDatum) {
+            this.log('IF timestamp:', this.HuffPoSports + ' = ' + 'HuffPoSportsDatum:', HuffPoSportsDatum);
+            this.HuffPoSports = HuffPoSportsDatum;
+            console.log('IF Title:', HuffPoSportsTitle);
+            console.log('IF timestamp update:', this.HuffPoSports);
 
             //trigger flow
             let tokens = {
-                'title': TweakersGamesTitel,
-                'description': TweakersGamesDescription
+                'title': HuffPoSportsTitle,
+                'description': HuffPoSportsDescription
             }
 
-            return this.TriggerTweakersGames
+            return this.TriggerHuffPoSports
                 .trigger(tokens)
                 .catch(this.error)
                 .then(this.log)
 
         } else {
-            this.log('ELSE timestamp:', this.TweakersGames + ' = ' + 'TweakersGamesDatum:', TweakersGamesDatum);
+            this.log('ELSE timestamp:', this.HuffPoSports + ' = ' + 'HuffPoSportsDatum:', HuffPoSportsDatum);
         }
 
     }
 
-    //Trigger TweakersVraagAanbod
-    async GetTweakersVraagAanbod() {
+    //Trigger HuffPoBooks
+    async GetHuffPoBooks() {
         try {
 
-            const url = 'https://tweakers.net/feeds/va.xml';
+            const url = 'http://www.huffingtonpost.com/section/books/feed';
 
-            this.log('Start GetTweakersVraagAanbod');
+            this.log('Start GetHuffPoBooks');
 
             var items = await feedparser.parse(url);
-            var TweakersVraagAanbodTitel = items[0].title;
-            var TweakersVraagAanbodCategorie = items[0].categories;
-            var TweakersVraagAanbodDescription = items[0].description;
-            var TweakersVraagAanbodDatumTZ = items[0].date;
-            var TweakersVraagAanbodDatum = Date.parse(TweakersVraagAanbodDatumTZ) / 1000
-            this.log('End Success GetTweakersVraagAanbod');
+            var HuffPoBooksTitle = items[0].title;
+            var HuffPoBooksDescription = items[0].description;
+            var HuffPoBooksDatumTZ = items[0].pubDate;
+            var HuffPoBooksDatum = Date.parse(HuffPoBooksDatumTZ) / 1000
+            this.log('End Success GetHuffPoBooks');
 
         } catch (error) {
             console.error('error: ', error)
-            this.log('End Error GetTweakersVraagAanbod');
+            this.log('End Error GetHuffPoBooks');
         }
 
-        if (this.TweakersVraagAanbod !== TweakersVraagAanbodDatum) {
-            this.log('IF timestamp:', this.TweakersVraagAanbod + ' = ' + 'TweakersVraagAanbodDatum:', TweakersVraagAanbodDatum);
-            this.TweakersVraagAanbod = TweakersVraagAanbodDatum;
-            console.log('IF titel:', TweakersVraagAanbodTitel);
-            console.log('IF timestamp update:', this.TweakersVraagAanbod);
+        if (this.HuffPoBooks !== HuffPoBooksDatum) {
+            this.log('IF timestamp:', this.HuffPoBooks + ' = ' + 'HuffPoBooksDatum:', HuffPoBooksDatum);
+            this.HuffPoBooks = HuffPoBooksDatum;
+            console.log('IF Title:', HuffPoBooksTitle);
+            console.log('IF timestamp update:', this.HuffPoBooks);
 
             //trigger flow
             let tokens = {
-                'title': TweakersVraagAanbodTitel,
-                'description': TweakersVraagAanbodDescription
+                'title': HuffPoBooksTitle,
+                'description': HuffPoBooksDescription
             }
 
-            return this.TriggerTweakersVraagAanbod
+            return this.TriggerHuffPoBooks
                 .trigger(tokens)
                 .catch(this.error)
                 .then(this.log)
 
         } else {
-            this.log('ELSE timestamp:', this.TweakersVraagAanbod + ' = ' + 'TweakersVraagAanbodDatum:', TweakersVraagAanbodDatum);
+            this.log('ELSE timestamp:', this.HuffPoBooks + ' = ' + 'HuffPoBooksDatum:', HuffPoBooksDatum);
         }
 
     }
 
+    //Trigger HuffPoGreen
+    async GetHuffPoGreen() {
+        try {
 
+            const url = 'http://www.huffingtonpost.com/section/green/feed';
+
+            this.log('Start GetHuffPoGreen');
+
+            var items = await feedparser.parse(url);
+            var HuffPoGreenTitle = items[0].title;
+            var HuffPoGreenDescription = items[0].description;
+            var HuffPoGreenDatumTZ = items[0].pubDate;
+            var HuffPoGreenDatum = Date.parse(HuffPoGreenDatumTZ) / 1000
+            this.log('End Success GetHuffPoGreen');
+
+        } catch (error) {
+            console.error('error: ', error)
+            this.log('End Error GetHuffPoGreen');
+        }
+
+        if (this.HuffPoGreen !== HuffPoGreenDatum) {
+            this.log('IF timestamp:', this.HuffPoGreen + ' = ' + 'HuffPoGreenDatum:', HuffPoGreenDatum);
+            this.HuffPoGreen = HuffPoGreenDatum;
+            console.log('IF Title:', HuffPoGreenTitle);
+            console.log('IF timestamp update:', this.HuffPoGreen);
+
+            //trigger flow
+            let tokens = {
+                'title': HuffPoGreenTitle,
+                'description': HuffPoGreenDescription
+            }
+
+            return this.TriggerHuffPoGreen
+                .trigger(tokens)
+                .catch(this.error)
+                .then(this.log)
+
+        } else {
+            this.log('ELSE timestamp:', this.HuffPoGreen + ' = ' + 'HuffPoGreenDatum:', HuffPoGreenDatum);
+        }
+
+    }
+	
 }
 
-module.exports = Tweakers;
+module.exports = Huffpost;
